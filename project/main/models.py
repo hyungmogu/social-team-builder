fc180fd8b8b8603213ae233f01a3630ce7f2948b
Hyungmo Gu <guhyungm7@gmail.com>
2019-08-11 12:45:29 -0700

    feat: Add project model

--- project/main/models.py
+++ project/main/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Project(models.Model):
+    title = models.CharField(max_length=255)
+    user = models.ForeignKey('auth.User')
+    needs = models.ManyToManyField('Position')
+    timeline = models.CharField(max_length=255)
+    applicant_requirements = models.CharField(max_length=255)
+    description = models.TextField()
