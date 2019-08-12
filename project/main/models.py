5416e1c74da88af2bcec056503b3a96ec2603626
Hyungmo Gu <guhyungm7@gmail.com>
2019-08-11 18:06:05 -0700

    feat: Define str rep. of classes for mTm Fields

--- project/main/models.py
+++ project/main/models.py
@@ -11,12 +11,21 @@ class Project(models.Model):
     applicant_requirements = models.CharField(max_length=255)
     description = models.TextField()
 
+    def __str__(self):
+        return self.title
+
 
 class Position(models.Model):
     name = models.CharField(max_length=255)
     description = models.TextField()
     related_skills = models.ManyToManyField('Skill')
 
+    def __str__(self):
+        return self.name
+
 
 class Skill(models.Model):
     name = models.CharField(max_length=255, unique=True)
+
+    def __str__(self):
+        return self.name
