2cfbd75c4aea3739e06df9d1e6ddee6ac349099b
Hyungmo Gu <guhyungm7@gmail.com>
2019-08-13 21:20:09 -0700

    feat: Add endpoint '/accounts/login/'

--- project/accounts/urls.py
+++ project/accounts/urls.py
@@ -4,7 +4,7 @@ from . import views
 
 # API endpoints
 urlpatterns = [
-    url(r'^login/$', views.login, name="login"),
+    url(r'^login/$', views.LoginView.as_view(), name="login"),
     url(r'^logout/$', views.logout, name="logout"),
     url(r'^sign_up/$', views.sign_up, name="sign_up"),
 ]
--- project/accounts/views.py
+++ project/accounts/views.py
@@ -1,11 +1,24 @@
 from django.shortcuts import render
+from django.contrib.auth.forms import AuthenticationForm, UserCreationForm
+from django.core.urlresolvers import reverse, reverse_lazy
+from django.views.generic import FormView
+
 
 # Create your views here.
 
+class LoginView(FormView):
+    form_class = AuthenticationForm
+    success_url = reverse_lazy('profile')
+    template_name = 'accounts/signin.html'
 
+    def get_form(self, form_class=None):
+        if form_class is None:
+            form_class = self.get_form_class()
+        return form_class(self.request, **self.get_form_kwargs())
 
-def login(request):
-    return render(request, 'accounts/signin.html')
+    def form_valid(self, form):
+        login(self.request, form.get_user())
+        return super().form_valid(form)
 
 def logout(request):
     pass
