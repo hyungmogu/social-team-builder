62d92d723e93d207eda8d758e414d811515a74ff
Hyungmo Gu <guhyungm7@gmail.com>
2019-08-13 21:08:34 -0700

    test: Add tests for User model

--- project/main/tests.py
+++ project/main/tests.py
@@ -14,6 +14,57 @@ from .models import Project, Skill, Position
 # -----------
 
 """
+USER MODEL
+"""
+class UserModelTestCase(TestCase):
+    def setUp(self):
+        self.user1 = User.objects.create(
+            username='hello',
+            password='hello'
+        )
+
+        self.user2 = User.objects.create(
+            username='world',
+            password='world'
+        )
+
+    def test_return_user_model_with_query_count_of_2(self):
+        expected = 2
+
+        result = User.objects.all().count()
+
+        self.assertEqual(expected, result)
+
+    def test_return_hello_as_username_and_password_when_queried_with_pk_of_1(self):
+        expected_username = 'hello'
+        expected_password = 'hello'
+
+        user = User.objects.get(pk=1)
+        result_username = user.username
+        result_password = user.password
+
+        self.assertEqual(expected_username, result_username)
+        self.assertEqual(expected_password, result_password)
+
+    def test_return_world_as_username_and_password_when_queried_with_pk_of_2(self):
+        expected_username = 'world'
+        expected_password = 'world'
+
+        user = User.objects.get(pk=2)
+        result_username = user.username
+        result_password = user.password
+
+        self.assertEqual(expected_username, result_username)
+        self.assertEqual(expected_password, result_password)
+
+    def test_return_username_when_queried_object_is_type_casted(self):
+        expected = 'hello'
+
+        result = str(User.objects.get(pk=1))
+
+        self.assertEqual(expected, result)
+
+"""
 SKILL MODEL
 """
 class TestSkillModel(TestCase):
@@ -341,8 +392,12 @@ class TestProjectModel(TestCase):
 
 
 
+# -----------
+# VIEW TESTS
+# -----------
+
 """
-VIEW MODEL
+/projects/create
 """
 class ProjectCreateGETTestCase(TestCase):
     def setUp(self):
