42c01f496c09bc7ffae786905ba51f8ecb4b477a
Hyungmo Gu <guhyungm7@gmail.com>
2019-08-15 23:23:00 -0700

    feat: Add custom user model

--- project/accounts/models.py
+++ project/accounts/models.py
@@ -1,3 +1,45 @@
 from django.db import models
+from django.contrib.auth.base_user import BaseUserManager
+from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin
 
-# Create your models here.
+
+class UserManager(BaseUserManager):
+    def create_user(self, email, password=None):
+        if not email:
+            raise ValueError("User must have a valid email address")
+
+        user = self.model(
+            email=self.normalize_email(email)
+        )
+
+        user.set_password(password)
+        user.save()
+        return user
+
+    def create_superuser(self, email, password):
+        user = self.create_user(
+            email,
+            password
+        )
+
+        user.is_staff = True
+        user.is_superuser = True
+        user.save()
+
+        return user
+
+
+class User (AbstractBaseUser, PermissionsMixin):
+    email = models.EmailField(unique=True)
+    is_employer = models.BooleanField(default=False)
+
+    objects = UserManager()
+
+    USERNAME_FIELD = "email"
+    REQUIRED_FIELDS = []
+
+    def __str__(self):
+        return "@{}".format(self.email)
+
+    def get_short_name(self):
+        return self.display_name
