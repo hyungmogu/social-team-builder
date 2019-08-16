ebf4ba6c2b26767dbfde06fe2ef86569f513bddc
Hyungmo Gu <guhyungm7@gmail.com>
2019-08-16 08:17:09 -0700

    feat: Add endpt. logics for '/search/by_position'

--- project/main/views.py
+++ project/main/views.py
@@ -3,7 +3,7 @@ from django.core.urlresolvers import reverse, reverse_lazy
 from django.forms.models import modelformset_factory
 from django.views.generic import (
     TemplateView, DetailView, UpdateView,
-    CreateView, DeleteView)
+    CreateView, DeleteView, ListView)
 
 from . import models, forms
 # Create your views here.
@@ -126,6 +126,25 @@ def profile_edit(request):
 
 def search(request):
     return render(request, 'main/search.html')
+
+class SearchByPositionView(ListView):
+    model = models.Project
+    template_name = 'main/home.html'
+
+    def get(self, request):
+        # 1. fetch query params
+        position = request.GET.get('q', '')
+
+        # 2. filter projects by position
+        projects = self.model.objects.filter(positions__name=position)
+        project_needs = models.Position.objects.order_by('name').distinct()
+
+        # 3. display result on search / home page
+        return render(request, self.template_name, {
+            'projects': projects,
+            'project_needs': project_needs,
+            'search_position': position
+        })
 
 def applications(request, pk):
     return render(request, 'main/applications.html')
