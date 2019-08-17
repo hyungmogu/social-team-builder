a3ecea5075964ad240d551dd80a160cafdffb072
Hyungmo Gu <guhyungm7@gmail.com>
2019-08-16 19:51:48 -0700

    feat: Add url for '/search'

--- project/main/urls.py
+++ project/main/urls.py
@@ -7,10 +7,11 @@ urlpatterns = [
     url(r'^projects/(?P<pk>\d+)/delete/$', views.ProjectDeleteView.as_view(), name="project_delete"),
     url(r'^projects/(?P<pk>\d+)/edit/$', views.ProjectEditView.as_view(), name="project_edit"),
     url(r'^projects/(?P<pk>\d+)/$', views.ProjectDetailView.as_view(), name="project"),
+    url(r'^projects/create/$', views.ProjectCreateView.as_view(), name="project_create"),
     url(r'^profile/edit/$', views.ProjectEditView.as_view(), name="profile_edit"),
     url(r'^profile/$', views.profile, name="profile"),
-    url(r'^projects/create/$', views.ProjectCreateView.as_view(), name="project_create"),
     url(r'^search/by_position/$', views.SearchByPositionView.as_view(), name="search_position"),
+    url(r'^search/$', views.SearchView.as_view(), name="search"),
     url(r'^applications/(?P<pk>\d+)/$', views.applications, name="application"),
     url(r'^$', views.HomeView.as_view(), name="home"),
 ]
