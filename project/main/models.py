84e555a57235e4b50455a9d8bc64d511dfa116c7
Hyungmo Gu <guhyungm7@gmail.com>
2019-08-16 23:10:45 -0700

    feat: Update Profile model

--- project/main/models.py
+++ project/main/models.py
@@ -9,18 +9,22 @@ class Project(models.Model):
     timeline = models.CharField(max_length=255)
     applicant_requirements = models.TextField()
     description = models.TextField()
+    end_date = models.DateField()
 
     def __str__(self):
         return self.title
 
+class UserProject(models.Model):
+    name= models.CharField(max_length=255)
+    url = models.URLField()
+    profile = ForeignKey('Profile', related_name="user_projects")
+
 class Profile(models.Model):
-    user = models.ForeignKey(settings.AUTH_USER_MODEL)
+    user = models.OneToOneField(settings.AUTH_USER_MODEL, related_name='profile')
     first_name = models.CharField(max_length=255)
     last_name = models.CharField(max_length=255)
-    projects = models.ForeignKey(Project)
     short_bio = models.TextField(blank=True)
     profile_image = models.ImageField(upload_to='profile_images', null=True, blank=True)
-    skills = models.TextField()
 
     def __str__(self):
         return self.name
@@ -37,6 +41,7 @@ class Position(models.Model):
 
 class Skill(models.Model):
     name = models.CharField(max_length=255, unique=True)
+    profile = ForeignKey('Profile', related_name="skills")
 
     def __str__(self):
         return self.name
