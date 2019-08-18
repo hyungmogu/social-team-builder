5091b14f422ab14ad3aecbc3037c13bd26209d83
Hyungmo Gu <guhyungm7@gmail.com>
2019-08-18 16:12:38 -0700

    refactor: Create suburls from '/applications'

--- project/main/urls.py
+++ project/main/urls.py
@@ -14,6 +14,9 @@ urlpatterns = [
     url(r'^search/by_position/$', views.SearchByPositionView.as_view(), name="search_position"),
     url(r'^search/$', views.SearchView.as_view(), name="search"),
     url(r'^applications/applicants/(?P<pk>\d+)/edit/$', views.ApplicantEditView.as_view(), name="applicant_edit"),
+    url(r'^applications/by_proj_need/$', views.ApplicationsByProjectNeedView.as_view(), name="applications_proj_need"),
+    url(r'^applications/by_project/$', views.ApplicationsByProjectView.as_view(), name="applications_project"),
+    url(r'^applications/by_status/$', views.ApplicationsByStatusView.as_view(), name="applications_status"),
     url(r'^applications/$', views.ApplicationsView.as_view(), name="applications"),
     url(r'^$', views.HomeView.as_view(), name="home"),
 ]
