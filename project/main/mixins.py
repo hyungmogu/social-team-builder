6da6f6a694b259ec3a44db1cc7b68409f2ce11f7
Hyungmo Gu <guhyungm7@gmail.com>
2019-08-24 20:56:18 -0700

    refactor: Improve consistency 'mixins.py'

--- project/main/mixins.py
+++ project/main/mixins.py
@@ -3,32 +3,38 @@ from django.shortcuts import redirect, get_object_or_404
 from django.core.urlresolvers import reverse
 
 class ProjectMustBeAuthorMixin:
-    def get(self, request, *args, **kwargs):
-        project = models.Project.objects.get(pk=self.kwargs.get('pk'))
+    def dispatch(self, request, *args, **kwargs):
+        project = get_object_or_404(
+            models.Project,
+            pk=self.kwargs.get('pk'))
 
         if request.user.pk != project.user.pk:
             return redirect(reverse('project', kwargs={
                 'pk': project.pk
             }))
 
-        return super().get(request, *args, **kwargs)
+        return super().dispatch(request, *args, **kwargs)
 
 
 class ProfileMustBeAuthorMixin:
-    def get(self, request, *args, **kwargs):
-        profile = models.Profile.objects.get(pk=self.kwargs.get('pk'))
+    def dispatch(self, request, *args, **kwargs):
+        profile = get_object_or_404(
+            models.Profile,
+            pk=self.kwargs.get('pk'))
 
         if request.user.pk != profile.user.pk:
             return redirect(reverse('profile', kwargs={
                 'pk': profile.pk
             }))
 
-        return super().get(request, *args, **kwargs)
+        return super().dispatch(request, *args, **kwargs)
 
 
 class ApplicationMustBeForAuthorMixin:
     def dispatch(self, request, *args, **kwargs):
-        application = get_object_or_404(models.Application, pk=self.kwargs.get('pk'))
+        application = get_object_or_404(
+            models.Application,
+            pk=self.kwargs.get('pk'))
 
         if request.user.pk != application.project.user.pk:
             return redirect(reverse('applications'))
