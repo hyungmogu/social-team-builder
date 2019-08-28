f0f3505c5f244c8ab6ece0e9c3eb0953a0526e46
Hyungmo Gu <guhyungm7@gmail.com>
2019-08-27 23:26:34 -0700

    Add setup files to pythonanywhere

---
+++ project/project/deploy.py
@@ -0,0 +1,23 @@
+"""
+WSGI config for project project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/1.9/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+from whitenoise.django import DjangoWhiteNoise
+
+path = "/home/__ACOUNT__/social-team-builder/project/"
+if path not in sys.path:
+    sys.path.append(path)
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "project.settings")
+
+application = get_wsgi_application()
+application = DjangoWhiteNoise(application)
